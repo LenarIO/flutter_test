bf19f6b5df24a3d19531600c5dd2161052d88dd0
LenarIO <hair.leo@gmail.com>
2025-02-01 09:49:46 +0300

    Add Text Branch New future

--- lib/main.dart
+++ lib/main.dart
@@ -53,7 +53,7 @@ class _MyHomePageState extends State<MyHomePage> {
             const Text(
               'You have pushed the button this many times:',
             ),
-            Text('This is TEST'),
+            Text('Это ветка NEW FUTURE'),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
